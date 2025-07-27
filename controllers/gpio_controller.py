737deebb83f815d33f6470a43b58fe8336eac7ae
begues12 <beeguespark@gmail.com>
2025-07-27 18:18:07 +0200

    as

--- controllers/gpio_controller.py
+++ controllers/gpio_controller.py
@@ -50,7 +50,7 @@ class GPIOController:
                 try:
                     from machine_config import config_manager
                     door_config = config_manager.get_door(door_id)
-                    active_high = door_config.get('active_high', True)
+                    active_high = door_config.get('active_high', False)
                     self.dispensers[door_id] = OutputDevice(pin, active_high=active_high, initial_value=False)
                     self.dispensers[door_id].off()  # Apagar relé al iniciar
                     logger.info(f"Dispensador OutputDevice configurado: {door_id} -> Pin {pin} (active_high={active_high})")
